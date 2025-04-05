f75128dba2e83783dbf1db424712138918ec069c
ViktorSvertoka <victor.svertoka@gmail.com>
2025-04-05 16:26:32 +0300

    feat(files): complete task 2

--- task2.js
+++ task2.js
@@ -5,3 +5,30 @@
 // відобразити у тезі #userCity
 // Запустити програму потрібно за допомогою Live Server
 // Перевірити правильність програми - команда node tests/task2.test.js
+
+document.getElementById('getUserButton').addEventListener('click', async () => {
+  const userName = document.getElementById('userNameInput').value.trim();
+  const userCitySpan = document.getElementById('userCity');
+
+  if (userName === '') {
+    userCitySpan.textContent = 'Please enter a user name.';
+    return;
+  }
+
+  try {
+    const response = await fetch('https://jsonplaceholder.typicode.com/users');
+    const users = await response.json();
+
+    const user = users.find(
+      userSearch => userSearch.name.toLowerCase() === userName.toLowerCase()
+    );
+
+    if (user) {
+      userCitySpan.textContent = `User's city: ${user.address.city}`;
+    } else {
+      userCitySpan.textContent = 'User not found.';
+    }
+  } catch (error) {
+    userCitySpan.textContent = 'Error fetching data.';
+  }
+});
